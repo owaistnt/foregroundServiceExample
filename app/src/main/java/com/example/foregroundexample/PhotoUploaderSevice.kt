c2d6aa69324616b50438c0aae7c060e548e80eda
Owais Idris <owais.i@peopleinteractive.in>
2019-08-18 21:08:45 +0530

    Added Progress notification

--- app/src/main/java/com/example/foregroundexample/PhotoUploaderSevice.kt
+++ app/src/main/java/com/example/foregroundexample/PhotoUploaderSevice.kt
@@ -5,25 +5,75 @@ import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.os.Build
+import android.os.CountDownTimer
 import android.os.IBinder
+import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 
 class PhotoUploaderSevice : Service() {
 
     val NOTIFICATION_CHANNEL="photo_uploader_service"
 
+    val notificationId: Int
+    init {
+        notificationId=111;
+    }
+
+    var notificationManager: NotificationManager?=null
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         createNotificationChannel()
-        val notification=NotificationCompat.Builder(this, NOTIFICATION_CHANNEL).apply {
+        notificationManager=ContextCompat.getSystemService(this, NotificationManager::class.java)
+        val notification= getSimpleNotification().build()
+
+        startForeground(notificationId, notification)
+        val timer=object: CountDownTimer(10000, 1000){
+            override fun onFinish() {
+                    val updateNotification=getProgressNotification().apply { setProgress(100, 50, true) }.build()
+                    notificationManager?.notify(notificationId, updateNotification)
+            }
+
+            override fun onTick(p0: Long) {
+               val updatedNotificationBundle= getProgressNotification().apply {
+                   val progress=(((10000-p0)*100)/10000).toInt()
+                   Log.d("TEST", "Millis: $p0 Progress: $progress")
+                    setProgress(100, progress,  false)
+                }
+                notificationManager?.notify(notificationId, updatedNotificationBundle.build())
+
+            }
+
+        }
+
+        timer.start()
+
+        return START_NOT_STICKY
+    }
+
+    private fun getSimpleNotification(): NotificationCompat.Builder {
+       return  NotificationCompat.Builder(this, NOTIFICATION_CHANNEL).apply {
             setContentTitle("Uploading  Photos")
             setContentText("initializing...")
             setSmallIcon(R.drawable.ic_cloud_upload_black_24dp)
+        }
+    }
 
-        }.build()
 
-        startForeground(1, notification)
-        return START_NOT_STICKY
+    private fun getProgressNotification(): NotificationCompat.Builder {
+        return  NotificationCompat.Builder(this, NOTIFICATION_CHANNEL).apply {
+            setContentTitle("Uploading  Photos")
+            setContentText("progress..")
+            setSmallIcon(R.drawable.ic_cloud_upload_black_24dp)
+
+        }
     }
 
 
